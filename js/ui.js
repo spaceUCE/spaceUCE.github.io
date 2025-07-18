bb963597dbb64ed49a13938ed2ddb9a57fcbb586
spaceUCE <spaceUCE@gmail.com>
2025-07-18 05:17:00 -0500

    Delete js directory

--- js/ui.js
+++
@@ -1,5 +0,0 @@
-
-export function renderFormattedWeatherData(weatherData) {
-    const weatherDisplay = document.getElementById("weatherData");
-    weatherDisplay.textContent = JSON.stringify(weatherData, null, 2);
-}
